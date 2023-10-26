5656750a320d7360cdef16f0c2d0089eb821cd36
reymondzzzz <starkov.kirill123@gmail.com>
2023-10-26 11:36:53 +0300

    fix cache key in one line context

--- src/completion_cache.rs
+++ src/completion_cache.rs
@@ -130,7 +130,17 @@ pub fn cache_key_from_post(
 
 
 pub fn cache_part2_from_post(post: &CodeCompletionPost) -> String {
-    if post.inputs.multiline { "multiline".to_string() } else { "singleline".to_string() }
+    let mut key = format!("{};{};{};",
+                          post.inputs.cursor.file.clone(),
+                          post.inputs.cursor.line,
+                          post.inputs.cursor.character,
+    );
+    if post.inputs.multiline {
+        key += "multiline;";
+    } else {
+        key += "singleline;";
+    }
+    key
 }
 
 
