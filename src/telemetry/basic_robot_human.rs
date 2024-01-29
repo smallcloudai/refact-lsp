9860b43d4dcb134eeadda9215079a65018054441
Valeryi <ponyparser@gmail.com>
2024-01-29 15:18:06 +0000

    just cleaning around...

--- src/telemetry/basic_robot_human.rs
+++ src/telemetry/basic_robot_human.rs
@@ -1,19 +1,15 @@
 use std::collections::HashMap;
-use tracing::{debug, error, info};
-use std::sync::{Arc, RwLockWriteGuard};
-use std::sync::RwLock as StdRwLock;
-use std::path::PathBuf;
+use tracing::{info};
+use std::sync::{Arc, RwLockReadGuard};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
 
 use tokio::sync::RwLock as ARwLock;
 
 use crate::global_context;
 use crate::telemetry::utils;
-use crate::telemetry::telemetry_structs;
-use crate::telemetry::telemetry_structs::{SnippetTracker, TeleRobotHumanAccum};
-
+use crate::telemetry::telemetry_structs::{SnippetTracker, Storage, TeleRobotHumanAccum};
+use crate::telemetry::utils::compress_tele_records_to_file;
 
 
 pub fn create_robot_human_record_if_not_exists(
@@ -55,7 +51,7 @@ fn basetext_to_text_leap_calculations(
 
     // let real_characters_added = re.replace_all(&added_characters, "").len() as i64 - re.replace_all(&removed_characters, "").len() as i64;
     let human_characters = re.replace_all(&added_characters, "").len() as i64 - rec.robot_characters_acc_baseline;
-    debug!("human_characters: +{}; robot_characters: +{}", human_characters, rec.robot_characters_acc_baseline);
+    info!("human_characters: +{}; robot_characters: +{}", human_characters, rec.robot_characters_acc_baseline);
     rec.human_characters += 0.max(human_characters);
     rec.robot_characters += rec.robot_characters_acc_baseline;
     rec.robot_characters_acc_baseline = 0;
@@ -88,8 +84,21 @@ pub fn increase_counters_from_finished_snippet(
     }
 }
 
+pub fn on_did_close_text_leap_calculations(
+    tele_robot_human: &mut Vec<TeleRobotHumanAccum>,
+    uri: &String,
+    text: &String,
+) {
+    let now = chrono::Local::now().timestamp();
+    if let Some(rec) = tele_robot_human.iter_mut().find(|stat| stat.uri.eq(uri)) {
+        basetext_to_text_leap_calculations(rec, rec.baseline_text.clone(), text);
+        rec.baseline_updated_ts = now;
+        rec.baseline_text = text.clone();
+    }
+}
+
 fn compress_robot_human(
-    storage_locked: &mut RwLockWriteGuard<telemetry_structs::Storage>
+    storage_locked: &RwLockReadGuard<Storage>
 ) -> Vec<TeleRobotHuman> {
     let mut unique_combinations: HashMap<(String, String), Vec<TeleRobotHumanAccum>> = HashMap::new();
 
@@ -119,48 +128,20 @@ fn compress_robot_human(
 pub async fn tele_robot_human_compress_to_file(
     cx: Arc<ARwLock<global_context::GlobalContext>>,
 ) {
-    let now = chrono::Local::now();
-    let cache_dir: PathBuf;
-    let storage: Arc<StdRwLock<telemetry_structs::Storage>>;
-    let enduser_client_version;
-    let file_prefix;
-    let mut records = json!([]);
-    {
-        let cx_locked = cx.read().await;
-        storage = cx_locked.telemetry.clone();
-        cache_dir = cx_locked.cache_dir.clone();
-        enduser_client_version = cx_locked.cmdline.enduser_client_version.clone();
-        file_prefix = cx_locked.cmdline.get_prefix();
-
-        let mut storage_locked = storage.write().unwrap();
-        for rec in compress_robot_human(&mut storage_locked) {
-            if rec.model.is_empty() && rec.robot_characters == 0 && rec.human_characters == 0 {
-                continue;
-            }
-            let json_dict = serde_json::to_value(rec).unwrap();
-            records.as_array_mut().unwrap().push(json_dict);
+    let mut records = vec![];
+    for rec in compress_robot_human(&cx.read().await.telemetry.read().unwrap()) {
+        if rec.model.is_empty() && rec.robot_characters == 0 && rec.human_characters == 0 {
+            continue;
         }
-        storage_locked.tele_robot_human.clear();
+        let json_dict = serde_json::to_value(rec).unwrap();
+        records.push(json_dict);
     }
-    if records.as_array().unwrap().is_empty() {
-        info!("no robot_human telemetry to save");
-        return;
-    }
-    let (dir, _) = utils::telemetry_storage_dirs(&cache_dir).await;
-
-    let fn_rh = dir.join(format!("{}-{}-rh.json", file_prefix, now.format("%Y%m%d-%H%M%S")));
-    let big_json_rh = json!({
-        "records": records,
-        "ts_start": now.timestamp(),
-        "ts_end": now.timestamp(),
-        "teletype": "robot_human",
-        "enduser_client_version": enduser_client_version,
-    });
-    info!("robot_human telemetry save \"{}\"", fn_rh.to_str().unwrap());
-    let io_result = utils::file_save(fn_rh.clone(), big_json_rh).await;
-    if io_result.is_err() {
-        error!("error: {}", io_result.err().unwrap());
-    }
+    match compress_tele_records_to_file(cx.clone(), records, "robot_human".to_string(), "rh".to_string()).await {
+        Ok(_) => {
+            cx.write().await.telemetry.write().unwrap().tele_robot_human.clear();
+        },
+        Err(_) => {}
+    };
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
