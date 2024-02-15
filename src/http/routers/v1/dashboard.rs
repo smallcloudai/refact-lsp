970cb66a74f883b38eebac53a2dcfb588f0cb3e0
Valeryi <ponyparser@gmail.com>
2024-02-15 17:28:47 +0000

    changed name in caps to telemetry_basic_retrieve_my_own

--- src/http/routers/v1/dashboard.rs
+++ src/http/routers/v1/dashboard.rs
@@ -73,7 +73,7 @@ pub async fn get_dashboard_plots(
     };
     let (api_key, url) = {
         let gcx_locked = global_context.read().await;
-        (gcx_locked.cmdline.api_key.clone(), caps.read().unwrap().telemetry_get_my_own_stats.clone())
+        (gcx_locked.cmdline.api_key.clone(), caps.read().unwrap().telemetry_basic_retrieve_my_own.clone())
     };
     if url.is_empty() {
         return Err(ScratchError::new(StatusCode::INTERNAL_SERVER_ERROR, "Error: no url provided from caps".to_string()));
