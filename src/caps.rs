cc848281cfd340429b08c4bfb90830ab8f7c3cc7
Valeryi <ponyparser@gmail.com>
2024-02-29 18:20:45 +0000

    init

--- src/caps.rs
+++ src/caps.rs
@@ -1,17 +1,15 @@
 use tracing::{info, error};
-use serde::Deserialize;
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 use std::fs::File;
 use std::collections::HashMap;
 use std::io::Read;
-use std::sync::Arc;
-use std::sync::RwLock as StdRwLock;
-use tokio::sync::RwLock;
+use std::sync::{RwLock as StdRwLock, Arc};
+use axum::http::header::AUTHORIZATION;
+use tokio::sync::RwLock as ARwLock;
+
 use url::Url;
 use crate::global_context::GlobalContext;
 use crate::known_models::KNOWN_MODELS;
-
-const CAPS_FILENAME: &str = "coding_assistant_caps.json";
 
 
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
@@ -44,6 +42,8 @@ pub struct CodeAssistantCaps {
     #[serde(default)]
     pub telemetry_basic_retrieve_my_own: String,
     #[serde(default)]
+    pub known_models_url: String,
+    #[serde(default)]
     pub telemetry_corrected_snippets_dest: String,
     #[serde(default)]
     pub code_completion_models: HashMap<String, ModelRecord>,
@@ -68,73 +68,28 @@ pub struct CodeAssistantCaps {
 
 pub async fn load_caps(
     cmdline: crate::global_context::CommandLine,
-    global_context: Arc<RwLock<GlobalContext>>,
+    global_context: Arc<ARwLock<GlobalContext>>,
 ) -> Result<Arc<StdRwLock<CodeAssistantCaps>>, String> {
-    let mut buffer = String::new();
-    let mut is_local_file = false;
-    let mut is_remote_address = false;
-    let caps_url: String;
-    if cmdline.address_url == "Refact" {
-        is_remote_address = true;
-        caps_url = "https://inference.smallcloud.ai/coding_assistant_caps.json".to_string();
-    } else if cmdline.address_url == "HF" {
-        buffer = HF_DEFAULT_CAPS.to_string();
-        caps_url = "<compiled-in-caps-hf>".to_string();
-    } else {
-        if cmdline.address_url.starts_with("http") {
-            is_remote_address = true;
-            let base_url = Url::parse(&cmdline.address_url.clone()).map_err(|_| "failed to parse address url (1)".to_string())?;
-            let joined_url = base_url.join(&CAPS_FILENAME).map_err(|_| "failed to parse address url (2)".to_string())?;
-            caps_url = joined_url.to_string();
-        } else {
-            is_local_file = true;
-            caps_url = cmdline.address_url.clone();
-        }
-    }
-    if is_local_file {
-        let mut file = File::open(caps_url.clone()).map_err(|_| format!("failed to open file '{}'", caps_url))?;
-        file.read_to_string(&mut buffer).map_err(|_| format!("failed to read file '{}'", caps_url))?;
-    }
-    if is_remote_address {
-        let api_key = cmdline.api_key.clone();
-        let http_client = global_context.read().await.http_client.clone();
-        let mut headers = reqwest::header::HeaderMap::new();
-        if !api_key.is_empty() {
-            headers.insert(reqwest::header::AUTHORIZATION, reqwest::header::HeaderValue::from_str(format!("Bearer {}", api_key).as_str()).unwrap());
-        }
-        let response = http_client.get(caps_url.clone()).headers(headers).send().await.map_err(|e| format!("{}", e))?;
-        let status = response.status().as_u16();
-        buffer = response.text().await.map_err(|e| format!("failed to read response: {}", e))?;
-        if status != 200 {
-            return Err(format!("server responded with: {}", buffer));
-        }
-    }
-    info!("reading caps from {}", caps_url);
-    let r0: ModelsOnly = serde_json::from_str(&KNOWN_MODELS).map_err(|e| {
-        let up_to_line = KNOWN_MODELS.lines().take(e.line()).collect::<Vec<&str>>().join("\n");
-        error!("{}\nfailed to parse KNOWN_MODELS: {}", up_to_line, e);
-        format!("failed to parse KNOWN_MODELS: {}", e)
-    })?;
-    let mut r1: CodeAssistantCaps = serde_json::from_str(&buffer).map_err(|e| {
-        let up_to_line = buffer.lines().take(e.line()).collect::<Vec<&str>>().join("\n");
-        error!("{}\nfailed to parse {}: {}", up_to_line, caps_url, e);
-        format!("failed to parse {}: {}", caps_url, e)
-    })?;
-    _inherit_r1_from_r0(&mut r1, &r0);
-    r1.endpoint_template = relative_to_full_url(&caps_url, &r1.endpoint_template)?;
-    r1.endpoint_chat_passthrough = relative_to_full_url(&caps_url, &r1.endpoint_chat_passthrough)?;
-    r1.telemetry_basic_dest = relative_to_full_url(&caps_url, &r1.telemetry_basic_dest)?;
-    r1.telemetry_corrected_snippets_dest = relative_to_full_url(&caps_url, &r1.telemetry_corrected_snippets_dest)?;
-    r1.telemetry_basic_retrieve_my_own = relative_to_full_url(&caps_url, &r1.telemetry_basic_retrieve_my_own)?;
-    r1.endpoint_embeddings_template = relative_to_full_url(&caps_url, &r1.endpoint_embeddings_template)?;
-    r1.tokenizer_path_template = relative_to_full_url(&caps_url, &r1.tokenizer_path_template)?;
-    info!("caps {} completion models", r1.code_completion_models.len());
-    info!("caps default completion model: \"{}\"", r1.code_completion_default_model);
-    info!("caps {} chat models", r1.code_chat_models.len());
-    info!("caps default chat model: \"{}\"", r1.code_chat_default_model);
-    Ok(Arc::new(StdRwLock::new(r1)))
-}
+    let (mut caps, caps_url) = fetch_caps(cmdline, global_context.clone()).await?;
 
+    caps.known_models_url = relative_to_full_url(&caps_url, &caps.known_models_url)?;
+    let models_only = load_known_models(global_context.clone(), &caps.known_models_url).await?;
+    complete_caps_with_models_only(&mut caps, &models_only);
+
+    caps.endpoint_template = relative_to_full_url(&caps_url, &caps.endpoint_template)?;
+    caps.endpoint_chat_passthrough = relative_to_full_url(&caps_url, &caps.endpoint_chat_passthrough)?;
+    caps.telemetry_basic_dest = relative_to_full_url(&caps_url, &caps.telemetry_basic_dest)?;
+    caps.telemetry_corrected_snippets_dest = relative_to_full_url(&caps_url, &caps.telemetry_corrected_snippets_dest)?;
+    caps.telemetry_basic_retrieve_my_own = relative_to_full_url(&caps_url, &caps.telemetry_basic_retrieve_my_own)?;
+    caps.endpoint_embeddings_template = relative_to_full_url(&caps_url, &caps.endpoint_embeddings_template)?;
+    caps.tokenizer_path_template = relative_to_full_url(&caps_url, &caps.tokenizer_path_template)?;
+
+    info!("caps {} completion models", caps.code_completion_models.len());
+    info!("caps default completion model: \"{}\"", caps.code_completion_default_model);
+    info!("caps {} chat models", caps.code_chat_models.len());
+    info!("caps default chat model: \"{}\"", caps.code_chat_default_model);
+    Ok(Arc::new(StdRwLock::new(caps)))
+}
 
 fn relative_to_full_url(
     caps_url: &String,
@@ -151,41 +106,41 @@ fn relative_to_full_url(
     }
 }
 
-fn _inherit_r1_from_r0(
-    r1: &mut CodeAssistantCaps,
-    r0: &ModelsOnly,
+fn complete_caps_with_models_only(
+    caps: &mut CodeAssistantCaps,
+    models_only: &ModelsOnly,
 ) {
-    // inherit models from r0, only if not already present in r1
-    for k in r0.code_completion_models.keys() {
-        if !r1.code_completion_models.contains_key(k) {
-            r1.code_completion_models.insert(k.to_string(), r0.code_completion_models[k].clone());
+    // inherit models from models_only, only if not already present in caps
+    for k in models_only.code_completion_models.keys() {
+        if !models_only.code_completion_models.contains_key(k) {
+            caps.code_completion_models.insert(k.to_string(), models_only.code_completion_models[k].clone());
         }
     }
-    for k in r0.code_chat_models.keys() {
-        if !r1.code_chat_models.contains_key(k) {
-            r1.code_chat_models.insert(k.to_string(), r0.code_chat_models[k].clone());
+    for k in models_only.code_chat_models.keys() {
+        if !caps.code_chat_models.contains_key(k) {
+            caps.code_chat_models.insert(k.to_string(), models_only.code_chat_models[k].clone());
         }
     }
     // clone to "similar_models"
-    let ccmodel_keys_copy = r1.code_completion_models.keys().cloned().collect::<Vec<String>>();
+    let ccmodel_keys_copy = caps.code_completion_models.keys().cloned().collect::<Vec<String>>();
     for k in ccmodel_keys_copy {
-        let model_rec = r1.code_completion_models[&k].clone();
+        let model_rec = caps.code_completion_models[&k].clone();
         for similar_model in model_rec.similar_models.iter() {
-            r1.code_completion_models.insert(similar_model.to_string(), model_rec.clone());
+            caps.code_completion_models.insert(similar_model.to_string(), model_rec.clone());
         }
     }
-    let chatmodel_keys_copy = r1.code_chat_models.keys().cloned().collect::<Vec<String>>();
+    let chatmodel_keys_copy = caps.code_chat_models.keys().cloned().collect::<Vec<String>>();
     for k in chatmodel_keys_copy {
-        let model_rec = r1.code_chat_models[&k].clone();
+        let model_rec = caps.code_chat_models[&k].clone();
         for similar_model in model_rec.similar_models.iter() {
-            r1.code_chat_models.insert(similar_model.to_string(), model_rec.clone());
+            caps.code_chat_models.insert(similar_model.to_string(), model_rec.clone());
         }
     }
-    r1.code_completion_models = r1.code_completion_models.clone().into_iter().filter(|(k, _)| r1.running_models.contains(&k)).collect();
-    r1.code_chat_models = r1.code_chat_models.clone().into_iter().filter(|(k, _)| r1.running_models.contains(&k)).collect();
+    caps.code_completion_models = caps.code_completion_models.clone().into_iter().filter(|(k, _)| caps.running_models.contains(&k)).collect();
+    caps.code_chat_models = caps.code_chat_models.clone().into_iter().filter(|(k, _)| caps.running_models.contains(&k)).collect();
 
-    for k in r1.running_models.iter() {
-        if !r1.code_completion_models.contains_key(k) && !r1.code_chat_models.contains_key(k) {
+    for k in caps.running_models.iter() {
+        if !caps.code_completion_models.contains_key(k) && !caps.code_chat_models.contains_key(k) {
             info!("indicated as running, unknown model {}", k);
         }
     }
@@ -197,17 +152,13 @@ pub fn which_model_to_use<'a>(
     default_model: &str,
 ) -> Result<(String, &'a ModelRecord), String> {
     let mut take_this_one = default_model;
-    if user_wants_model != "" {
+    if !user_wants_model.is_empty() {
         take_this_one = user_wants_model;
     }
-    if let Some(model_rec) = models.get(take_this_one) {
-        return Ok((take_this_one.to_string(), model_rec));
+    return if let Some(model_rec) = models.get(take_this_one) {
+        Ok((take_this_one.to_string(), model_rec))
     } else {
-        return Err(format!(
-            "Model '{}' not found. Server has these models: {:?}",
-            take_this_one,
-            models.keys()
-        ));
+        Err(format!("Model '{}' not found. Server has these models: {:?}", take_this_one, models.keys()))
     }
 }
 
@@ -221,25 +172,103 @@ pub fn which_scratchpad_to_use<'a>(
         take_this_one = user_wants_scratchpad;
     }
     if default_scratchpad == "" {
-        if scratchpads.len() == 1 {
+        return if scratchpads.len() == 1 {
             let key = scratchpads.keys().next().unwrap();
-            return Ok((key.clone(), &scratchpads[key]));
+            Ok((key.clone(), &scratchpads[key]))
         } else {
-            return Err(format!(
+            Err(format!(
                 "There is no default scratchpad defined, requested scratchpad is empty. The model supports these scratchpads: {:?}",
                 scratchpads.keys()
-            ));
+            ))
         }
     }
-    if let Some(scratchpad_patch) = scratchpads.get(take_this_one) {
-        return Ok((take_this_one.to_string(), scratchpad_patch));
+    return if let Some(scratchpad_patch) = scratchpads.get(take_this_one) {
+        Ok((take_this_one.to_string(), scratchpad_patch))
     } else {
-        return Err(format!(
+        Err(format!(
             "Scratchpad '{}' not found. The model supports these scratchpads: {:?}",
             take_this_one,
             scratchpads.keys()
-        ));
+        ))
     }
+}
+
+async fn fetch_caps(
+    cmdline: crate::global_context::CommandLine,
+    global_context: Arc<ARwLock<GlobalContext>>,
+) -> Result<(CodeAssistantCaps, String), String> {
+    let caps_filename = "coding_assistant_caps.json";
+
+    let mut buffer = String::new();
+    let mut is_local_file = false;
+    let mut is_remote_address = false;
+    let caps_url: String;
+    if cmdline.address_url == "Refact" {
+        is_remote_address = true;
+        caps_url = "https://inference.smallcloud.ai/coding_assistant_caps.json".to_string();
+    } else if cmdline.address_url == "HF" {
+        buffer = HF_DEFAULT_CAPS.to_string();
+        caps_url = "<compiled-in-caps-hf>".to_string();
+    } else {
+        if cmdline.address_url.starts_with("http") {
+            is_remote_address = true;
+            let base_url = Url::parse(&cmdline.address_url.clone()).map_err(|_| "failed to parse address url (1)".to_string())?;
+            let joined_url = base_url.join(&caps_filename).map_err(|_| "failed to parse address url (2)".to_string())?;
+            caps_url = joined_url.to_string();
+        } else {
+            is_local_file = true;
+            caps_url = cmdline.address_url.clone();
+        }
+    }
+    if is_local_file {
+        let mut file = File::open(caps_url.clone()).map_err(|_| format!("failed to open file '{}'", caps_url))?;
+        file.read_to_string(&mut buffer).map_err(|_| format!("failed to read file '{}'", caps_url))?;
+    }
+    if is_remote_address {
+        let api_key = cmdline.api_key.clone();
+        let http_client = global_context.read().await.http_client.clone();
+        let mut headers = reqwest::header::HeaderMap::new();
+        if !api_key.is_empty() {
+            headers.insert(AUTHORIZATION, reqwest::header::HeaderValue::from_str(format!("Bearer {}", api_key).as_str()).unwrap());
+        }
+        let response = http_client.get(caps_url.clone()).headers(headers).send().await.map_err(|e| format!("{}", e))?;
+        let status = response.status().as_u16();
+        buffer = response.text().await.map_err(|e| format!("failed to read response: {}", e))?;
+        if status != 200 {
+            return Err(format!("server responded with: {}", buffer));
+        }
+    }
+    let caps: CodeAssistantCaps = serde_json::from_str(&buffer).map_err(|e| {
+        let up_to_line = buffer.lines().take(e.line()).collect::<Vec<&str>>().join("\n");
+        error!("{}\nfailed to parse {}: {}", up_to_line, caps_url, e);
+        format!("failed to parse {}: {}", caps_url, e)
+    })?;
+    Ok((caps, caps_url))
+}
+
+async fn load_known_models(global_context: Arc<ARwLock<GlobalContext>>, known_models_url: &String) -> Result<ModelsOnly, String>  {
+    async fn fetch_known_models_from_url(global_context: Arc<ARwLock<GlobalContext>>, known_models_url: &String) -> Result<ModelsOnly, String> {
+        if known_models_url.is_empty() {
+            return Err("known_models_url is empty".to_string());
+        }
+        let cx = global_context.read().await;
+        let req = cx.http_client.get(known_models_url)
+            .header(AUTHORIZATION, format!("Bearer {}", cx.cmdline.api_key));
+        let res = req.send().await.map_err(|e| format!("failed to get response: {}", e))?;
+        let res = res.error_for_status().map_err(|e| format!("failed to read response: {}", e))?;
+        let text = res.text().await.map_err(|e| format!("failed to read response: {}", e))?;
+        let models_only = serde_json::from_str(&text).map_err(|e| format!("failed to parse models: {}", e));
+        info!("loaded known models from URL: {}", known_models_url);
+        models_only
+    }
+    fetch_known_models_from_url(global_context, known_models_url).await.or_else(|e| {
+        error!("failed to fetch known models from URL: {}; trying to read from disk", e);
+        serde_json::from_str(&KNOWN_MODELS).map_err(|e| {
+            let up_to_line = KNOWN_MODELS.lines().take(e.line()).collect::<Vec<&str>>().join("\n");
+            error!("{}\nfailed to parse KNOWN_MODELS: {}", up_to_line, e);
+            format!("failed to parse KNOWN_MODELS: {}", e)
+        })
+    })
 }
 
 const HF_DEFAULT_CAPS: &str = r#"
