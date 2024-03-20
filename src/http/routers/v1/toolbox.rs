b1da7cb1febc1dc4f3009f9cdb8b704a2ae63b7f
Valeryi <ponyparser@gmail.com>
2024-03-20 17:04:13 +0000

    init
    fix -- _load_and_mix_with_users_config
    
    no request for customization -- customization is a part of caps
    
    no tabs; rust-fmt
    
    fixed customization_web_mb

--- src/http/routers/v1/toolbox.rs
+++ src/http/routers/v1/toolbox.rs
@@ -1,17 +1,16 @@
 use axum::response::Result;
 use axum::Extension;
-use serde_json::json;
-use serde::{Serialize, Deserialize};
 use hyper::{Body, Response, StatusCode};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock as ARwLock;
-use std::collections::HashMap;
 use tracing::error;
 
 use crate::call_validation::ChatMessage;
-use crate::global_context::GlobalContext;
 use crate::custom_error::ScratchError;
-
+use crate::global_context::GlobalContext;
 
 pub async fn handle_v1_customization(
     Extension(global_context): Extension<Arc<ARwLock<GlobalContext>>>,
@@ -19,73 +18,88 @@ pub async fn handle_v1_customization(
 ) -> Result<Response<Body>, ScratchError> {
     let gcx: Arc<ARwLock<GlobalContext>> = global_context.clone();
     let cache_dir = gcx.read().await.cache_dir.clone();
-	let tconfig = match crate::toolbox::toolbox_config::load_customization_high_level(cache_dir) {
-		Ok(config) => config,
-		Err(err) => {
-			error!("load_customization_high_level: {}", err);
-			return Ok(Response::builder()
-				.status(StatusCode::INTERNAL_SERVER_ERROR)
-				.body(Body::from(serde_json::to_string_pretty(&json!({ "detail": err.to_string() })).unwrap()))
-				.unwrap());
-		}
-	};
+
+    let caps =
+        crate::global_context::try_load_caps_quickly_if_not_present(global_context.clone(), 0)
+            .await?;
+
+    let customization_web = caps.read().unwrap().customization.clone();
+    let customization_web_mb = if customization_web.is_empty() {
+        None
+    } else {
+        Some(customization_web)
+    };
+
+    let tconfig = match crate::toolbox::toolbox_config::load_customization_high_level(
+        cache_dir,
+        customization_web_mb,
+    ) {
+        Ok(config) => config,
+        Err(err) => {
+            error!("load_customization_high_level: {}", err);
+            return Ok(Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(Body::from(
+                    serde_json::to_string_pretty(&json!({ "detail": err.to_string() })).unwrap(),
+                ))
+                .unwrap());
+        }
+    };
     Ok(Response::builder()
         .status(StatusCode::OK)
         .body(Body::from(serde_json::to_string_pretty(&tconfig).unwrap()))
         .unwrap())
 }
 
-
 #[derive(Serialize, Deserialize, Clone)]
 struct SnippetAcceptedPostData {
     pub messages: Vec<ChatMessage>,
 }
 
-
 pub async fn handle_v1_rewrite_assistant_says_to_at_commands(
-	Extension(_global_context): Extension<Arc<ARwLock<GlobalContext>>>,
-	body_bytes: hyper::body::Bytes,
+    Extension(_global_context): Extension<Arc<ARwLock<GlobalContext>>>,
+    body_bytes: hyper::body::Bytes,
 ) -> Result<Response<Body>, ScratchError> {
-    let post = serde_json::from_slice::<SnippetAcceptedPostData>(&body_bytes).map_err(|e| {
-	    ScratchError::new(StatusCode::BAD_REQUEST, format!("JSON problem: {}", e))
-	})?;
+    let post = serde_json::from_slice::<SnippetAcceptedPostData>(&body_bytes)
+        .map_err(|e| ScratchError::new(StatusCode::BAD_REQUEST, format!("JSON problem: {}", e)))?;
 
-	let mut assistant_says = String::new();
-	let mut auto_response_map = HashMap::<String, String>::new();
-	let mut auto_response_str = String::new();
-	let mut original_toolbox_command = String::new();
-	for msg in post.messages.iter() {
-		if msg.role == "ignore" {
-	    	// msg.content="original-toolbox-command why\nauto-reponse 👣PROVIDE_COMMANDS_STEP 👣GENERATE_DOCUMENTATION_STEP\n"
-	    	for line in msg.content.lines() {
-	    		if line.starts_with("original-toolbox-command ") {
-	    			original_toolbox_command = line.replace("original-toolbox-command ", "").to_string();
-	    		}
-	    		if line.starts_with("auto-reponse ") {
-	    			let parts: Vec<&str> = line.split_whitespace().collect();
-	    			if parts.len() != 3 {
-	    				error!("auto-reponse has wrong format: {}", line);
-	    				continue;
-	    			} else {
-	    				auto_response_map.insert(parts[1].to_string(), parts[2].to_string());
-	    			}
-	    		}
-	    	}
-		}
-		if msg.role == "assistant" {
-			assistant_says = msg.content.clone();
-		}
-		if msg.role == "user" {
-			auto_response_str = "".to_string();
-			for user_says_line in msg.content.lines() {
-		        for (k, v) in auto_response_map.iter() {
-		        	if user_says_line.starts_with(k) {
-	        			auto_response_str = v.clone();
-	        		}
-	        	}
-	        }
-		}
-	}
+    let mut assistant_says = String::new();
+    let mut auto_response_map = HashMap::<String, String>::new();
+    let mut auto_response_str = String::new();
+    let mut original_toolbox_command = String::new();
+    for msg in post.messages.iter() {
+        if msg.role == "ignore" {
+            // msg.content="original-toolbox-command why\nauto-reponse 👣PROVIDE_COMMANDS_STEP 👣GENERATE_DOCUMENTATION_STEP\n"
+            for line in msg.content.lines() {
+                if line.starts_with("original-toolbox-command ") {
+                    original_toolbox_command =
+                        line.replace("original-toolbox-command ", "").to_string();
+                }
+                if line.starts_with("auto-reponse ") {
+                    let parts: Vec<&str> = line.split_whitespace().collect();
+                    if parts.len() != 3 {
+                        error!("auto-reponse has wrong format: {}", line);
+                        continue;
+                    } else {
+                        auto_response_map.insert(parts[1].to_string(), parts[2].to_string());
+                    }
+                }
+            }
+        }
+        if msg.role == "assistant" {
+            assistant_says = msg.content.clone();
+        }
+        if msg.role == "user" {
+            auto_response_str = "".to_string();
+            for user_says_line in msg.content.lines() {
+                for (k, v) in auto_response_map.iter() {
+                    if user_says_line.starts_with(k) {
+                        auto_response_str = v.clone();
+                    }
+                }
+            }
+        }
+    }
 
     let mut out = String::new();
     for s in assistant_says.lines() {
@@ -110,15 +124,18 @@ pub async fn handle_v1_rewrite_assistant_says_to_at_commands(
         }
     }
     if auto_response_str != "" {
-    	out += &auto_response_str.to_string();
+        out += &auto_response_str.to_string();
     }
     Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(Body::from(json!({
-        	"success": out!= "",
-        	"suggested_user_message": out,
-        	"auto_response": auto_response_str != "",
-        	"original_toolbox_command": original_toolbox_command,
-        }).to_string()))
+        .body(Body::from(
+            json!({
+                "success": out!= "",
+                "suggested_user_message": out,
+                "auto_response": auto_response_str != "",
+                "original_toolbox_command": original_toolbox_command,
+            })
+            .to_string(),
+        ))
         .unwrap())
 }
