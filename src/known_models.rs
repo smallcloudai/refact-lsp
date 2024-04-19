53d5e5c664afe776091fdb109ebd8e3cdf116a4c
Valeryi <ponyparser@gmail.com>
2024-04-19 17:58:33 +0100

    init
    Signed-off-by: Valeryi <ponyparser@gmail.com>

--- src/known_models.rs
+++ src/known_models.rs
@@ -105,16 +105,7 @@ pub const KNOWN_MODELS: &str = r####"
                 }
             },
             "similar_models": [
-            ]
-        },
-        "gpt-4": {
-            "n_ctx": 4096,
-            "supports_scratchpads": {
-                "PASSTHROUGH": {
-                    "default_system_message": "You are a coding assistant that outputs short answers, gives links to documentation."
-                }
-            },
-            "similar_models": [
+                "gpt-4"
             ]
         },
         "claude-instant-1.2": {
@@ -125,7 +116,10 @@ pub const KNOWN_MODELS: &str = r####"
                 }
             },
             "similar_models": [
-                "claude-2.1"
+                "claude-2.1",
+                "claude-3-haiku",
+                "claude-3-opus",
+                "claude-3-sonnet"
             ]
         },
         "starchat/15b/beta": {
