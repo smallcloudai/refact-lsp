98104cea8855aa518c7151138a0590043936e125
mitya <dimitry.ageev@gmail.com>
2024-06-10 17:33:49 +0300

    streaming function call using split_buffer

--- src/scratchpads/chat_utils_deltadelta.rs
+++ src/scratchpads/chat_utils_deltadelta.rs
@@ -1,5 +1,8 @@
 use uuid::Uuid;
 
+const FUNCTION_CALL_TAG_BEGIN: &str = "<functioncall>";
+const FUNCTION_CALL_TAG_END: &str = "</functioncall>";
+
 #[derive(Debug)]
 pub struct DeltaDeltaChatStreamer {
     // This class helps chat implementations to stop at two-token phrases (at most) when streaming,
@@ -9,6 +12,7 @@ pub struct DeltaDeltaChatStreamer {
     pub finished: bool,
     pub stop_list: Vec<String>,
     pub role: String,
+    pub is_function_call: bool,
 }
 
 impl DeltaDeltaChatStreamer {
@@ -18,6 +22,7 @@ impl DeltaDeltaChatStreamer {
             finished: false,
             stop_list: Vec::new(),
             role: String::new(),
+            is_function_call: false,
         }
     }
 
@@ -33,12 +38,10 @@ impl DeltaDeltaChatStreamer {
                 split_buffer_min_prefix(&x, &self.stop_list, true);
             let finished = stopped[i] | self.stop_list.contains(&delimiter);
 
-            let function_call_tag_begin = String::from("<functioncall>");
-            let function_call_tag_end = String::from("</functioncall>");
-            let (content, delimiter, postfix) =
-                split_buffer(&content, &function_call_tag_begin);
+            let (content, _, postfix) =
+                split_buffer(&content, &FUNCTION_CALL_TAG_BEGIN.to_string());
             let (function_call_text, _, _) =
-                split_buffer(&postfix, &function_call_tag_end);
+                split_buffer(&postfix, &FUNCTION_CALL_TAG_END.to_string());
 
             let mut message_json = serde_json::json!({
                 "role": self.role.clone(),
@@ -77,26 +80,78 @@ impl DeltaDeltaChatStreamer {
     {
         self.buffer += delta.as_str();
 
-        let (content, middle) =
+        let (mut content, mut middle) =
             split_buffer_min_prefix(&self.buffer, &self.stop_list, delta.is_empty());
-        let finished = stopped | self.stop_list.contains(&middle);
+        let mut finished = stopped | self.stop_list.contains(&middle);
+
         let mut finish_reason: serde_json::Value =
             if !delta.is_empty() { serde_json::Value::Null }
             else { serde_json::Value::String("length".to_string()) };
         if finished {
             finish_reason = serde_json::Value::String("stop".to_string());
         }
+
+        let mut function_call = serde_json::Value::Null;
+        if !self.is_function_call {
+            // trying to find that function call tag begins
+            let delimiter: String;
+            let postfix: String;
+            (content, delimiter, postfix) =
+                split_buffer(&content, &FUNCTION_CALL_TAG_BEGIN.to_string());
+            if delimiter == FUNCTION_CALL_TAG_BEGIN {
+                // immediately process function call parsing, skip previous content
+                content = postfix.clone();
+                self.is_function_call = true;
+            } else {
+                // we found partial function call tag, so we need to add it into buffer
+                middle = delimiter + middle.as_str();
+            }
+        }
+        if self.is_function_call {
+            // in function call mode we don't send any content, but hold it in buffer
+            let delimiter: String;
+            (middle, delimiter, _) =
+                split_buffer(&content, &FUNCTION_CALL_TAG_END.to_string());
+            // if function call tag found we need now to parse and send it's content
+            if delimiter == FUNCTION_CALL_TAG_END {
+                function_call = serde_json::from_str::<serde_json::Value>(&middle.as_str())
+                    .unwrap_or_else(|_| serde_json::Value::Null);
+                function_call = function_call
+                    .as_array().unwrap().iter().enumerate()
+                    .map(|(index, item)| {
+                        serde_json::json!({
+                            "id": Uuid::new_v4().to_string(),
+                            "function": item,
+                            "type": "function",
+                            "index": 0,
+                        })
+                    }).collect();
+                finish_reason = serde_json::Value::String("tool_calls".to_string());
+                finished |= true;
+            }
+        }
+
         self.buffer = middle;
         self.finished = finished;
+
+        let json_delta: serde_json::Value;
+        if self.is_function_call {
+            json_delta = serde_json::json!({
+                "role": self.role.clone(),
+                "tool_calls": function_call,
+            });
+        } else {
+            json_delta = serde_json::json!({
+                "role": self.role.clone(),
+                "content": content.clone(),
+            });
+        }
 
         let ans = serde_json::json!({
             "choices": [
                 {
                     "index": 0,
-                    "delta": {
-                        "role": self.role.clone(),
-                        "content": content.clone(),
-                    },
+                    "delta": json_delta,
                     "finish_reason": finish_reason,
                 }
             ]
