fd2c2fe0c95b876328fc2c97e7846f6529b88b3a
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2024-06-12 14:42:21 +0200

    feat: adding http urls to the documentation

--- src/at_tools/att_doc_sources_add.rs
+++ src/at_tools/att_doc_sources_add.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
-use std::fs;
-use std::path::PathBuf;
+use std::fs::{self, File};
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
 use async_trait::async_trait;
 use serde_json::Value;
@@ -12,6 +13,21 @@ use crate::files_in_workspace::Document;
 
 pub struct AttDocSourcesAdd;
 
+async fn download_and_convert_page(url: &str) -> Result<String, String> {
+    let html = reqwest::get(url)
+        .await
+        .map_err(|_| format!("Unable to connect to '{url}'"))?
+        .text()
+        .await
+        .map_err(|_| "Unable to convert page to text".to_string())?;
+
+    let text = html2text::config::plain()
+        .string_from_read(&html.as_bytes()[..], 200)
+        .map_err(|_| "Unable to convert html to text".to_string())?;
+
+    Ok(text)
+}
+
 #[async_trait]
 impl AtTool for AttDocSourcesAdd {
     async fn execute(
@@ -20,11 +36,28 @@ impl AtTool for AttDocSourcesAdd {
         tool_call_id: &String,
         args: &HashMap<String, Value>,
     ) -> Result<Vec<ContextEnum>, String> {
-        let source = match args.get("source") {
+        let mut source = match args.get("source") {
             Some(Value::String(s)) => s.clone(),
             Some(v) => return Err(format!("argument `source` is not a string: {:?}", v)),
             None => return Err("Missing source argument for doc_sources_add".to_string()),
         };
+
+        // if the source is a url, download the page and convert it to markdown
+        if source.starts_with("http://") || source.starts_with("https://") {
+            let page = download_and_convert_page(source.as_str()).await?;
+
+            let file_path = format!("./.refact/{}", source.split("://").nth(1).unwrap());
+            let directory = Path::new(&file_path).parent().unwrap();
+            fs::create_dir_all(directory)
+                .map_err(|_| format!("Unable to create directory {directory:?}"))?;
+
+            let mut file = File::create(&file_path)
+                .map_err(|_| format!("Unable to create file {file_path}"))?;
+
+            file.write_all(page.as_bytes())
+                .map_err(|_| format!("Unable to write to file {file_path}"))?;
+            source = file_path;
+        }
 
         let abs_source_path = PathBuf::from(source.as_str());
         if fs::canonicalize(abs_source_path).is_err() {
