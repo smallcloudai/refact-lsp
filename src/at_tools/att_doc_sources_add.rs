c98e09b680e5f0b287733b235835f66e7e14d54b
marcomeijer <46689298+MarcoMeijer@users.noreply.github.com>
2024-06-18 07:57:26 +0200

    feat: adding websites to documentation in background

--- src/at_tools/att_doc_sources_add.rs
+++ src/at_tools/att_doc_sources_add.rs
@@ -4,27 +4,32 @@ use std::fs::{self, File};
 use std::io::Write;
 use std::mem::swap;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 use async_trait::async_trait;
 use hashbrown::HashSet;
+use log::info;
 use select::predicate::Name;
 use serde_json::Value;
+use tokio::task;
+use tokio::sync::RwLock as ARwLock;
 use url::{Position, Url};
 
 use crate::at_commands::at_commands::AtCommandsContext;
 use crate::at_tools::tools::AtTool;
 use crate::call_validation::{ChatMessage, ContextEnum};
 use crate::files_in_workspace::Document;
+use crate::global_context::GlobalContext;
 
 pub struct AttDocSourcesAdd;
 
-async fn walk_links(url_str: &str, depth: usize, max_pages: usize) -> Result<Vec<String>, String> {
+async fn add_url_to_documentation(gcx: Arc<ARwLock<GlobalContext>>, url_str: String, depth: usize, max_pages: usize) -> Result<Vec<String>, String> {
     let mut visited_pages = HashSet::new();
     let mut queue = vec![];
-    let url = Url::parse(url_str).map_err(|_| format!("Invalid url '{url_str}'"))?;
+    let mut sources = vec![];
+    let url = Url::parse(&url_str).map_err(|_| format!("Invalid url '{url_str}'"))?;
     let base_url = Url::parse(&url[..Position::BeforePath])
         .map_err(|_| format!("Unable to find base url of '{url_str}'"))?;
-    let mut files = vec![];
 
     queue.push(url_str.to_string());
     visited_pages.insert(url_str.to_string());
@@ -54,7 +59,23 @@ async fn walk_links(url_str: &str, depth: usize, max_pages: usize) -> Result<Vec
                 .map_err(|_| format!("Unable to create file {file_path}"))?;
             file.write_all(text.as_bytes())
                 .map_err(|_| format!("Unable to write to file {file_path}"))?;
-            files.push(file_path);
+
+            // vectorize file
+            let gcx = gcx.write().await;
+            let mut files = gcx.documents_state.documentation_files.lock().await;
+            let vec_db_module = {
+                *gcx.documents_state.cache_dirty.lock().await = true;
+                gcx.vec_db.clone()
+            };
+            sources.push(file_path.clone());
+            if !files.contains(&file_path) {
+                files.push(file_path.clone());
+            }
+            let document = Document::new(&PathBuf::from(file_path.as_str()));
+            match *vec_db_module.lock().await {
+                Some(ref mut db) => db.vectorizer_enqueue_files(&vec![document], false).await,
+                None => {}
+            };
 
             if is_last_iteration {
                 continue;
@@ -85,7 +106,7 @@ async fn walk_links(url_str: &str, depth: usize, max_pages: usize) -> Result<Vec
         swap(&mut queue, &mut new_urls);
     }
 
-    Ok(files)
+    Ok(sources)
 }
 
 #[async_trait]
@@ -103,28 +124,28 @@ impl AtTool for AttDocSourcesAdd {
         };
 
         // if the source is an url, download the page and convert it to markdown
-        let mut sources = vec![];
         if source.starts_with("http://") || source.starts_with("https://") {
-            sources = walk_links(source.as_str(), 2, 40).await?;
+            task::spawn(add_url_to_documentation(ccx.global_context.clone(), source, 2, 40));
+            let results = vec![ContextEnum::ChatMessage(ChatMessage {
+                role: "tool".to_string(),
+                content: "Started background task to add website to documentation, this may take a few minutes...".to_string(),
+                tool_calls: None,
+                tool_call_id: tool_call_id.clone(),
+            })];
+            Ok(results)
         } else {
-            sources.push(source);
-        }
-
-        for source in &sources {
             let abs_source_path = PathBuf::from(source.as_str());
             if fs::canonicalize(abs_source_path).is_err() {
                 return Err(format!("File or directory '{}' doesn't exist", source));
             }
-        }
 
-        let gcx = ccx.global_context.write().await;
-        let mut files = gcx.documents_state.documentation_files.lock().await;
-        let vec_db_module = {
-            *gcx.documents_state.cache_dirty.lock().await = true;
-            gcx.vec_db.clone()
-        };
+            let gcx = ccx.global_context.write().await;
+            let mut files = gcx.documents_state.documentation_files.lock().await;
+            let vec_db_module = {
+                *gcx.documents_state.cache_dirty.lock().await = true;
+                gcx.vec_db.clone()
+            };
 
-        for source in sources {
             if !files.contains(&source) {
                 files.push(source.clone());
             }
@@ -133,14 +154,14 @@ impl AtTool for AttDocSourcesAdd {
                 Some(ref mut db) => db.vectorizer_enqueue_files(&vec![document], false).await,
                 None => {}
             };
-        }
 
-        let results = vec![ContextEnum::ChatMessage(ChatMessage {
-            role: "tool".to_string(),
-            content: "Successfully added source to documentation list.".to_string(),
-            tool_calls: None,
-            tool_call_id: tool_call_id.clone(),
-        })];
-        Ok(results)
+            let results = vec![ContextEnum::ChatMessage(ChatMessage {
+                role: "tool".to_string(),
+                content: "Successfully added source to documentation list.".to_string(),
+                tool_calls: None,
+                tool_call_id: tool_call_id.clone(),
+            })];
+            Ok(results)
+        }
     }
 }
