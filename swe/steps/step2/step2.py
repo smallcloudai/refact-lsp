9f9b0dbca9ae7a8c39aa071dd910ddc5589279ae
mitya <dimitry.ageev@gmail.com>
2024-08-06 00:04:14 +0300

    step2 context and todo improvements

--- swe/steps/step2/step2.py
+++ swe/steps/step2/step2.py
@@ -1,91 +1,194 @@
+import re
 import json
 import subprocess
 
-from swe.steps import Step
 from refact import chat_client
+from refact.chat_client import print_block
+from refact.chat_client import print_exception
+from refact.chat_client import print_messages
+from swe.steps import Step
 
+from collections import Counter
 from pathlib import Path
-from typing import Dict, Any, List, Set
+from typing import List, Set
 
 
-DONE_MESSAGE = "DONE"
-SYSTEM_MESSAGE = f"""
-You are Refact Dev, an auto coding assistant.
+CONTEXT_SYSTEM_MESSAGE = f"""
+You're Refact Dev a prefect AI assistant.
 
-You'll receive a problem statement from user.
-Your aim is to solve this problem using speculation over the code and analyzing outputs of given tools.
-Use tools to get access to the codebase. Use each tool exact in it's format do not add any extra args.
+You should collect all needed context to solve the problem.
+- Look through the user's problem statement and given files structure.
+- Collect additional context using definition and references tools if needed.
+- Call tools in parallel as much as it possible.
+"""
 
-A good strategy to solve the issue is:
-1. Build context:
- - use file, definition or references tools
- - before you move to the next step, make sure you collect all needed context: file names, code, etc.
-2. Speculate about the problem and solve it:
- - describe what changes you need to do
- - apply changes to files separately using patch tool
- - paths argument should be always full paths to given files within repo
- - do not generate the patch itself, use patch(paths, todo) tool to make this changes
-3. When you are done with the task, send the message including only one word: {DONE_MESSAGE}
+PATCH_SYSTEM_MESSAGE = f"""
+You're Refact Dev a prefect AI assistant.
 
-Changing tests is not allowed!
+You should solve the problem using given context and patch tool:
+- Choose relevant file to patch.
+- Introduce a plan that will solve the problem.
+- Simultaneously call the patch tool to produce a patch.
 
-Explain your plan briefly before calling the tools in parallel.
-IT IS FORBIDDEN TO JUST CALL TOOLS WITHOUT EXPLAINING. EXPLAIN FIRST! USE TOOLS IN PARALLEL!
+Rules of patch tool using:
+- Choose exact one filename to patch.
+- You should solve the problem with exact one patch tool call per message.
+- Patch command doesn't have your context so you need to pass all relevant symbols and write accurate todo.
+- Todo should contain the plan how to solve given problem with detailed description of each step and warnings about possible problems with solution.
+"""
+
+
+PATCH_TODO_REMINDER = f"""
+A reminder of patch generation:
+- Make sure you added all imports if it needed.
+- Do not add anything that is not related to the problem.
+- Your patch should be minimalistic, never try to add unnecessary code.
+- If it possible use native language objects.
+
+If you see that you can't solve the problem in given file with provided context just refuse patch generation!
 """
 
 
 class ProducePatchStep(Step):
-    def __init__(self, attempts: int, *args, **kwargs):
+
+    def __init__(self, context_choices: int, patch_choices: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._attempts = attempts
+        self._context_choices = context_choices
+        self._patch_choices = patch_choices
+        self._active_tools = set()
 
     @property
     def _tools(self) -> Set[str]:
-        return {
-            "file",
-            "definition",
-            "patch",
-        }
+        return self._active_tools
 
-    async def _patch_generate(self, repo_name: Path, formatted_diff: List[Dict[str, Any]]):
+    @staticmethod
+    def _extract_filenames(text: str, filter_tests: bool = False) -> Set[str]:
+        pattern = r'\b(?:[a-zA-Z]:\\|/)?(?:[\w-]+[/\\])*[\w-]+\.\w+\b'
+        filenames = set(re.findall(pattern, text))
+        if filter_tests:
+            filenames = {f for f in filenames if "test" not in f.lower()}
+        return filenames
+
+    async def _patch_generate(self, message: chat_client.Message, repo_name: Path):
+        if message.role != "diff":
+            raise RuntimeError("not a diff message")
+        formatted_diff = json.loads(message.content)
         await chat_client.diff_apply(self._base_url, chunks=formatted_diff, apply=[True] * len(formatted_diff))
         result = subprocess.check_output(["git", "--no-pager", "diff"], cwd=str(repo_name))
         await chat_client.diff_apply(self._base_url, chunks=formatted_diff, apply=[False] * len(formatted_diff))
-        # TODO: make sure that un-apply works well (possible freezes in refact-lsp and possible patches in repo after)
-        # subprocess.check_output(["git", "stash"], cwd=str(repo_name))
         return result.decode()
 
-    async def _single_step(self, message: str, repo_path: Path) -> str:
+    async def _patch(self, message: chat_client.Message, repo_name: Path, problem_statement: str) -> str:
+        function_dict = message.tool_calls[0].function
+        if function_dict.name != "patch":
+            raise RuntimeError("not a patch tool call")
+        args = json.loads(function_dict.arguments)
+        if len(args.get("paths", "").split(",")) != 1:
+            raise RuntimeError("patch tool call should edit exactly one filename")
+        if not args.get("todo", ""):
+            raise RuntimeError("patch tool should contain todo")
+        args["todo"] = "\n\n".join([
+            "Original problem:", problem_statement,
+            "Plan:", args["todo"],
+            PATCH_TODO_REMINDER,
+        ])
+        function_dict.arguments = json.dumps(args)
+        self._trajectory.extend(print_messages([message]))
+        patch_tool_messages = await self._query([message], only_deterministic_messages=True)
+        self._trajectory.extend(print_messages(patch_tool_messages))
+        for message in patch_tool_messages:
+            if message.role == "diff":
+                return await self._patch_generate(message, repo_name)
+        raise RuntimeError(f"expected a diff message")
+
+    async def _deterministic_tool_call_messages(
+            self, functions: List[chat_client.FunctionDict]) -> List[chat_client.Message]:
+        tool_calls = [
+            chat_client.ToolCallDict(id=chat_client.gen_function_call_id(), function=function, type='function')
+            for function in functions
+        ]
         messages = [
-            chat_client.Message(role="system", content=SYSTEM_MESSAGE),
-            chat_client.Message(role="user", content=message),
+            chat_client.Message(role="assistant", finish_reason="tool_calls", tool_calls=tool_calls),
+        ]
+        tool_messages = await self._query(messages, only_deterministic_messages=True)
+        return messages + tool_messages
+
+    async def _collect_context(
+            self,
+            problem_statement: str,
+            related_files: List[str],
+            repo_path: Path) -> List[chat_client.Message]:
+        self._active_tools = {
+            "definition",
+            "references",
+        }
+        messages = [
+            chat_client.Message(role="system", content=CONTEXT_SYSTEM_MESSAGE),
+            chat_client.Message(role="user", content=f"Problem statement:\n\n{problem_statement}"),
         ]
 
-        for step_n in range(self._max_depth):
-            print(f"{'-' * 40} step {step_n} {'-' * 40}")
-            messages = await self._query(messages)
-            applied_diff_call_ids = set()
-            for m in [m for m in messages if m.role == "diff" and m.tool_call_id not in applied_diff_call_ids]:
-                applied_diff_call_ids.add(m.tool_call_id)
-                try:
-                    formatted_diff = json.loads(m.content)
-                    return await self._patch_generate(repo_path.absolute(), formatted_diff)
-                except json.decoder.JSONDecodeError:
-                    continue
-            if messages[-1].role == "assistant" \
-                    and messages[-1].content \
-                    and DONE_MESSAGE == messages[-1].content:
-                break
-        raise RuntimeError(f"can't solve the problem with {self._max_depth} steps")
+        paths = ",".join([str(repo_path / filename) for filename in related_files])
+        messages.extend(
+            await self._deterministic_tool_call_messages([
+                chat_client.FunctionDict(arguments='{"paths":"' + paths + '"}', name='files_skeleton')
+            ])
+        )
+        self._trajectory.extend(print_messages(messages))
 
-    async def process(self, task: str, repo_path: Path, **kwargs) -> List[str]:
-        results = []
-        for attempt_n in range(self._attempts):
-            print(f"{'=' * 40} attempt {attempt_n} {'=' * 40}")
+        function_dict_counter = Counter()
+        for idx, new_messages in enumerate(await self._query_choices(messages, self._context_choices)):
+            self._trajectory.append(print_block("context choice", idx + 1))
+            self._trajectory.extend(print_messages(new_messages))
             try:
-                results.append(await self._single_step(task, repo_path))
-            except:
-                continue
-        if not results:
-            raise RuntimeError(f"can't produce result with {self._attempts} attempts")
+                def _normalize(args: str):
+                    try:
+                        return json.dumps(json.loads(args))
+                    except:
+                        return args
+
+                function_dict_counter.update([
+                    chat_client.FunctionDict(
+                        name=tool_call_dict.function.name,
+                        arguments=_normalize(tool_call_dict.function.arguments),
+                    )
+                    for tool_call_dict in new_messages[-1].tool_calls
+                    if tool_call_dict.type == "function"
+                ])
+            except Exception as e:
+                self._trajectory.append(print_exception(e, trace=True))
+
+        if function_dict_counter:
+            tool_call_messages = await self._deterministic_tool_call_messages([
+                function_dict for function_dict, _ in function_dict_counter.most_common()
+            ])
+            self._trajectory.extend(print_messages(tool_call_messages))
+            messages += tool_call_messages
+
+        return messages[1:]
+
+    async def _collect_patches(
+            self,
+            problem_statement: str,
+            repo_path: Path,
+            context_messages: List[chat_client.Message]):
+        self._active_tools = {
+            "patch",
+        }
+        messages = [
+            chat_client.Message(role="system", content=PATCH_SYSTEM_MESSAGE),
+            *context_messages,
+        ]
+        results = list()
+        for idx, new_messages in enumerate(await self._query_choices(messages, self._patch_choices)):
+            self._trajectory.append(print_block("patch choice", idx + 1))
+            self._trajectory.extend(print_messages(new_messages))
+            try:
+                results.append(await self._patch(new_messages[-1], repo_path.absolute(), problem_statement))
+            except Exception as e:
+                self._trajectory.append(print_exception(e, trace=True))
+        return results
+
+    async def process(self, problem_statement: str, related_files: List[str], repo_path: Path, **kwargs) -> List[str]:
+        context_messages = await self._collect_context(problem_statement, related_files, repo_path)
+        results = await self._collect_patches(problem_statement, repo_path, context_messages)
         return results
