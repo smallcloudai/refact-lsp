d6b04cf00e41d3f622f611ff15d5be17bdd093cc
mitya <dimitry.ageev@gmail.com>
2024-09-03 11:44:20 +0300

    step1 fixes

--- swe/steps/step1/step1_locate.py
+++ swe/steps/step1/step1_locate.py
@@ -1,7 +1,8 @@
 import ujson as json
-from refact import chat_client
-from refact.chat_client import print_messages
+
 from swe.steps import Step
+from refact.chat_client import FunctionDict
+from refact.chat_client import print_messages
 
 from pathlib import Path
 from typing import List, Dict, Any, Set
@@ -16,53 +17,9 @@ class Locate(Step):
         return set()
 
     async def process(self, problem_statement: str, repo_path: Path, **kwargs) -> Dict[str, Any]:
-
-        tool_args = {
-            "problem_statement": problem_statement,
-        }
-
-        tool_call_dict = chat_client.ToolCallDict(
-            id=chat_client.gen_function_call_id(),
-            function=chat_client.FunctionDict(arguments=json.dumps(tool_args), name='locate'),
-            type='function')
-
-        messages = [
-            chat_client.Message(role="assistant", finish_reason="tool_calls", tool_calls=[tool_call_dict]),
-        ]
-        self._trajectory.extend(print_messages(messages))
-
-        new_messages = await self._query(messages, only_deterministic_messages=True)
-        self._trajectory.extend(print_messages(new_messages))
-
-        res_message = [m for m in new_messages if m.role == "tool"][-1]
-        try:
-            results: Dict[str, List[Any]] = json.loads(res_message.content)
-        except Exception as e:
-            raise RuntimeError(f"content is not decodable as json:\n{res_message.content}\nError: {e}")
-
-        if 1:
-            # Oleg branch
-            # {
-            #   "filename": {
-            #     "SYMBOLS": "A,B,C",
-            #     "WHY_CODE": "DEFINITIONS",
-            #     "WHY_DESC": "Defines the index domain and its directives, crucial for understanding how index...",
-            #     "RELEVANCY": 5
-            #   },
-            # }
-            return results, []
-        else:
-            # Valerii branch
-            files_list = results.get('files')
-            symbols = results.get('symbols')
-            return files_list, symbols
-
-
-    async def __process(self, problem_statement: str, repo_path: Path, **kwargs) -> Dict[str, Any]:
         messages = await self._deterministic_tool_call_messages([
             FunctionDict(name="locate", arguments=json.dumps({
                 "problem_statement": problem_statement,
-                # "problem_statement": f"Problem statement:\n\n{problem_statement}",
             }))
         ])
         self._trajectory.extend(print_messages(messages))
@@ -100,3 +57,46 @@ class Locate(Step):
             "context_symbols": list(set(symbols_list)),
             "to_change_files": list(set(to_change_files)),
         }
+
+    # async def process(self, problem_statement: str, repo_path: Path, **kwargs) -> Dict[str, Any]:
+    #
+    #     tool_args = {
+    #         "problem_statement": problem_statement,
+    #     }
+    #
+    #     tool_call_dict = chat_client.ToolCallDict(
+    #         id=chat_client.gen_function_call_id(),
+    #         function=chat_client.FunctionDict(arguments=json.dumps(tool_args), name='locate'),
+    #         type='function')
+    #
+    #     messages = [
+    #         chat_client.Message(role="assistant", finish_reason="tool_calls", tool_calls=[tool_call_dict]),
+    #     ]
+    #     self._trajectory.extend(print_messages(messages))
+    #
+    #     new_messages = await self._query(messages, only_deterministic_messages=True)
+    #     self._trajectory.extend(print_messages(new_messages))
+    #
+    #     res_message = [m for m in new_messages if m.role == "tool"][-1]
+    #     try:
+    #         results: Dict[str, List[Any]] = json.loads(res_message.content)
+    #     except Exception as e:
+    #         raise RuntimeError(f"content is not decodable as json:\n{res_message.content}\nError: {e}")
+    #
+    #     if 1:
+    #         # Oleg branch
+    #         # {
+    #         #   "filename": {
+    #         #     "SYMBOLS": "A,B,C",
+    #         #     "WHY_CODE": "DEFINITIONS",
+    #         #     "WHY_DESC": "Defines the index domain and its directives, crucial for understanding how index...",
+    #         #     "RELEVANCY": 5
+    #         #   },
+    #         # }
+    #         return results, []
+    #     else:
+    #         # Valerii branch
+    #         files_list = results.get('files')
+    #         symbols = results.get('symbols')
+    #         return files_list, symbols
+    #
