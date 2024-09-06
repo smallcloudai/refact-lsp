3d0eff9640933e37aa608b0bf286c5e85d08a451
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-09-06 15:45:34 +0200

    feat/load-privacy-if-needed

--- src/privacy.rs
+++ src/privacy.rs
@@ -2,44 +2,47 @@ use std::sync::Arc;
 use std::path::Path;
 use serde::{Deserialize, Serialize};
 use tokio::sync::RwLock as ARwLock;
-use tokio::fs;
 use tokio::time::Duration;
+use tokio::fs;
 use tracing::{error, info};
 use glob::Pattern;
+use std::time::SystemTime;
 
 use crate::global_context::GlobalContext;
 
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq, PartialOrd)]
 pub enum FilePrivacyLevel {
-    AllowToSendEverywhere,
-    OnlySendToServersIControl,
-    Blocked,
+    AllowToSendEverywhere = 0,
+    OnlySendToServersIControl = 1,
+    Blocked = 2,
 }
-
-const PRIVACY_RELOAD_EACH_N_SECONDS: Duration = Duration::from_secs(1);
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct PrivacySettings {
     pub file_privacy: FilePrivacySettings,
+    #[serde(default = "default_expiry_time", skip)]
+    pub expiry_time: u64,
+}
+
+const PRIVACY_RELOAD_EACH_N_SECONDS: Duration = Duration::from_secs(10);
+fn default_expiry_time() -> u64 {
+    SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() + PRIVACY_RELOAD_EACH_N_SECONDS.as_secs()
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 pub struct FilePrivacySettings {
     #[serde(default)]
+    #[allow(non_snake_case)]
     pub only_send_to_servers_I_control: Vec<String>,
     #[serde(default)]
     pub blocked: Vec<String>,
 }
 
-pub async fn load_privacy(global_context: Arc<ARwLock<GlobalContext>>) -> PrivacySettings {
-    let cache_dir = global_context.read().await.cache_dir.clone();
-    let path = cache_dir.join("privacy.yaml");
-
+async fn read_privacy_yaml(path: &Path) -> PrivacySettings {
     match fs::read_to_string(&path).await {
         Ok(content) => {
             match serde_yaml::from_str(&content) {
                 Ok(privacy_settings) => {
-                    // log info that privacy settings were loaded correctly
                     info!("privacy settings loaded from {}", path.display());
                     privacy_settings
                 }
@@ -50,18 +53,29 @@ pub async fn load_privacy(global_context: Arc<ARwLock<GlobalContext>>) -> Privac
             }
         }
         Err(e) => {
-            error!("cannot read {}: {}, no privacy settings will be used", path.display(), e);
+            error!("failed to read content from {}: {}, no privacy settings will be used", path.display(), e);
             PrivacySettings::default()
         }
     }
 }
 
-pub async fn privacy_background_reload(global_context: Arc<ARwLock<GlobalContext>>) {
-    loop {
-        let privacy_settings = load_privacy(global_context.clone()).await;
-        global_context.write().await.privacy_settings = Arc::new(ARwLock::new(privacy_settings));
-        tokio::time::sleep(PRIVACY_RELOAD_EACH_N_SECONDS).await;
+async fn load_privacy_if_needed(global_context: Arc<ARwLock<GlobalContext>>) {
+    let (should_reload, path) = {
+        let global_context_lock = global_context.read().await;
+        let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+        let should_reload = global_context_lock.privacy_settings.expiry_time <= current_time;
+        let path = global_context_lock.cache_dir.join("privacy.yaml");
+        (should_reload, path)
+    };
+
+    if !should_reload {
+        return;
     }
+
+    let new_privacy_settings = read_privacy_yaml(&path).await;
+
+    let mut global_context_lock = global_context.write().await;
+    global_context_lock.privacy_settings = Arc::new(new_privacy_settings);
 }
 
 fn any_glob_matches_path(globs: &Vec<String>, path: &Path) -> bool {
@@ -71,9 +85,9 @@ fn any_glob_matches_path(globs: &Vec<String>, path: &Path) -> bool {
     })
 }
 
-pub async fn get_file_privacy_level(global_context: Arc<ARwLock<GlobalContext>>, path: &Path) -> FilePrivacyLevel {
+async fn get_file_privacy_level(global_context: Arc<ARwLock<GlobalContext>>, path: &Path) -> FilePrivacyLevel {
     let global_context_lock = global_context.read().await;
-    let privacy_settings = global_context_lock.privacy_settings.read().await;
+    let privacy_settings = &global_context_lock.privacy_settings;
 
     if any_glob_matches_path(&privacy_settings.file_privacy.blocked, path) {
         FilePrivacyLevel::Blocked
@@ -81,6 +95,17 @@ pub async fn get_file_privacy_level(global_context: Arc<ARwLock<GlobalContext>>,
         FilePrivacyLevel::OnlySendToServersIControl
     } else {
         FilePrivacyLevel::AllowToSendEverywhere
+    }
+}
+
+pub async fn check_file_privacy(global_context: Arc<ARwLock<GlobalContext>>, path: &Path, min_blocked_privacy_level: FilePrivacyLevel) -> Result<(), String> {
+    load_privacy_if_needed(global_context.clone()).await;
+
+    let file_privacy_level = get_file_privacy_level(global_context.clone(), path).await;
+    if file_privacy_level >= min_blocked_privacy_level {
+        Err(format!("File privacy level for file is too low, {} is {:?}", path.display(), file_privacy_level))
+    } else {
+        Ok(())
     }
 }
 
