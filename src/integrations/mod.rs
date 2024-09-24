935b799785c5bd2b4c1e1fac5fdeb75fd7f763c9
Oleg Klimov <omgtech@gmail.com>
2024-09-24 07:18:22 +0200

    docker init

--- src/integrations/mod.rs
+++ src/integrations/mod.rs
@@ -2,6 +2,7 @@ pub mod integr_github;
 pub mod integr_pdb;
 pub mod integr_chrome;
 pub mod sessions;
+pub mod integr_docker;
 pub mod process_io_utils;
 
 pub const INTEGRATIONS_DEFAULT_YAML: &str = r#"# This file is used to configure integrations in Refact Agent.
