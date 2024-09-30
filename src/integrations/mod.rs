99c51afb11d663ac23d398e878e436ed341ef399
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-09-30 10:42:55 +0200

    fix/update-default-integrations-yaml

--- src/integrations/mod.rs
+++ src/integrations/mod.rs
@@ -3,19 +3,18 @@ pub mod integr_github;
 pub const INTEGRATIONS_DEFAULT_YAML: &str = r#"# This file is used to configure integrations.
 #
 # If there is a syntax error in this file, integrations will not be loaded.
+#
+# Set rules to require confirmation or deny commands. If both apply, the command is denied.
+#
+# Rules use glob patterns for wildcard matching (https://en.wikipedia.org/wiki/Glob_(programming))
+#
+commands_need_confirmation:
+  - "gh * delete*"
+commands_deny:
+  - "gh auth token*"
 
-# This part is used to configure GitHub integration.
-#
-# By default, commands will ask for confirmation. You can set rules to skip confirmation or deny 
-# specific commands using glob patterns (https://en.wikipedia.org/wiki/Glob_(programming))
-#
-# `deny` overrides `skip_confirmation` if both match
+# GitHub integration configuration
 github:
-  GH_TOKEN: # Your GITHUB_TOKEN (see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens)
-  # gh_binary_path: # Uncomment to set a custom path for the gh binary, defaults to gh in PATH
-  skip_confirmation: 
-    - "{browse,search,status}*"
-    - "{repo,gist,issue,org,pr,project,release,cache,run,workflow,label,ruleset} {list,view}*"
-  deny: 
-    - "auth token*"
+  GH_TOKEN: # <YOUR_GITHUB_TOKEN_HERE> (see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens)
+  # gh_binary_path: # Uncomment to set a custom path for the gh binary, defaults to "gh"
 "#;
