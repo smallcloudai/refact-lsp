6673eeb3a1c48128943348a995eb33b31952927c
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-10-16 10:18:37 +0200

    Revert "feat/add-docker-image-id-to-at-commands-context"
    This reverts commit ac9a48ada3d4eb8834c4376d7b3fecb08e2dd6da.

--- src/at_commands/at_commands.rs
+++ src/at_commands/at_commands.rs
@@ -29,7 +29,6 @@ pub struct AtCommandsContext {
     pub pp_skeleton: bool,
     pub correction_only_up_to_step: usize,  // suppresses context_file messages, writes a correction message instead
     pub chat_id: String,
-    pub docker_image_id: String,
 
     pub at_commands: HashMap<String, Arc<AMutex<Box<dyn AtCommand + Send>>>>,  // a copy from static constant
     pub subchat_tool_parameters: IndexMap<String, SubchatParameters>,
@@ -47,7 +46,6 @@ impl AtCommandsContext {
         is_preview: bool,
         messages: Vec<ChatMessage>,
         chat_id: String,
-        docker_image_id: String,
     ) -> Self {
         let (tx, rx) = mpsc::unbounded_channel::<serde_json::Value>();
         AtCommandsContext {
@@ -60,7 +58,6 @@ impl AtCommandsContext {
             pp_skeleton: false,
             correction_only_up_to_step: 0,
             chat_id,
-            docker_image_id,
 
             at_commands: at_commands_dict(global_context.clone()).await,
             subchat_tool_parameters: IndexMap::new(),
