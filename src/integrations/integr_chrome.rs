29e9e7ca1d78e87e6f6b584a3cdc9b233c2be135
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-10-24 13:46:51 +0200

    fix: issues generated by rebase, passing style properly

--- src/integrations/integr_chrome.rs
+++ src/integrations/integr_chrome.rs
@@ -56,6 +56,8 @@ impl ToolChrome {
 
 #[async_trait]
 impl Tool for ToolChrome {
+    fn as_any(&self) -> &dyn Any { self }
+
     async fn tool_execute(
         &mut self,
         ccx: Arc<AMutex<AtCommandsContext>>,
