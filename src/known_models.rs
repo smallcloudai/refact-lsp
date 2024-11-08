98e074e2ca63d7f7f76ba12cbba42c601fa49b26
JegernOUTT <sergey.vakhreev@gmail.com>
2024-11-08 20:50:27 +1030

    Add new model configurations

--- src/known_models.rs
+++ src/known_models.rs
@@ -187,6 +187,43 @@ pub const KNOWN_MODELS: &str = r####"
                 "claude-3-5-sonnet-20240620"
             ]
         },
+        "groq-llama-3.1-8b": {
+            "n_ctx": 128000,
+            "supports_tools": false,
+            "supports_multimodality": false,
+            "supports_scratchpads": {
+                "PASSTHROUGH": {}
+            },
+            "similar_models": [
+                "groq-llama-3.1-70b",
+                "groq-llama-3.2-1b",
+                "groq-llama-3.2-3b",
+                "groq-llama-3.2-11b-vision",
+                "groq-llama-3.2-90b-vision"
+            ]
+        },        
+        "cerebras-llama3.1-8b": {
+            "n_ctx": 8192,
+            "supports_tools": false,
+            "supports_multimodality": false,
+            "supports_scratchpads": {
+                "PASSTHROUGH": {}
+            },
+            "similar_models": [
+                "cerebras-llama3.1-70b"
+            ]
+        },
+        "claude-3-5-sonnet": {
+            "n_ctx": 16384,
+            "supports_tools": true,
+            "supports_multimodality": true,
+            "supports_scratchpads": {
+                "PASSTHROUGH": {}
+            },
+            "similar_models": [
+                "claude-3-5-sonnet-20240620"
+            ]
+        },
         "llama3/8b/instruct": {
             "n_ctx": 4096,
             "supports_scratchpads": {
@@ -212,17 +249,6 @@ pub const KNOWN_MODELS: &str = r####"
                 "llama3.2/3b/instruct/vllm",
                 "llama3.2/1b/instruct",
                 "llama3.2/1b/instruct/vllm"
-            ]
-        },
-        "groq-llama-3.1-8b": {
-            "n_ctx": 128000,
-            "supports_tools": true,
-            "supports_scratchpads": {
-                "PASSTHROUGH": {
-                }
-            },
-            "similar_models": [
-                "groq-llama-3.1-70b"
             ]
         },
         "wizardlm/7b": {
@@ -331,7 +357,7 @@ pub const KNOWN_MODELS: &str = r####"
         "claude-3-5-sonnet":          "Xenova/claude-tokenizer",
         "claude-3-5-sonnet-20240620": "Xenova/claude-tokenizer",
         "groq-llama-3.1-8b":      "Xenova/Meta-Llama-3.1-Tokenizer",
-        "groq-llama-3.1-70b":     "Xenova/Meta-Llama-3.1-Tokenizer"
+        "cerebras-llama3.1-8b":     "Xenova/Meta-Llama-3.1-Tokenizer"
     }
 }
 "####;
