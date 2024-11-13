1395af62e64c843a096d0b5417416fdb8049d90e
mitya <dimitry.ageev@gmail.com>
2024-11-13 15:25:04 +0300

    add try_stop for integration services

--- src/integrations/sessions.rs
+++ src/integrations/sessions.rs
@@ -1,5 +1,6 @@
 use std::{any::Any, sync::Arc};
 use tokio::sync::RwLock as ARwLock;
+use std::future::Future;
 
 use crate::global_context::GlobalContext;
 
@@ -8,6 +9,7 @@ pub trait IntegrationSession: Any + Send + Sync
     fn as_any_mut(&mut self) -> &mut dyn Any;
 
     fn is_expired(&self) -> bool;
+    fn try_stop(&mut self) -> Box<dyn Future<Output = String> + Send + '_>;
 }
 
 pub fn get_session_hashmap_key(integration_name: &str, base_key: &str) -> String {
@@ -33,7 +35,9 @@ async fn remove_expired_sessions(gcx: Arc<ARwLock<GlobalContext>>) {
     {
         let mut gcx_locked = gcx.write().await;
         for key in expired_keys {
-            gcx_locked.integration_sessions.remove(&key);
+            if let Some(session) = gcx_locked.integration_sessions.remove(&key) {
+                Box::into_pin(session.lock().await.try_stop()).await;
+            }
         }
     }
     // sessions still keeps a reference on all sessions, just in case a destructor is called in the block above
@@ -45,5 +49,18 @@ pub async fn remove_expired_sessions_background_task(
     loop {
         tokio::time::sleep(tokio::time::Duration::from_secs(60)).await;
         remove_expired_sessions(gcx.clone()).await;
+    }
+}
+
+pub async fn stop_sessions(gcx: Arc<ARwLock<GlobalContext>>) {
+    let mut gcx_locked = gcx.write().await;
+    let keys = gcx_locked.integration_sessions.iter()
+        .map(|(key, _)| key.to_string())
+        .collect::<Vec<_>>();
+
+    for key in keys {
+        if let Some(session) = gcx_locked.integration_sessions.remove(&key) {
+            Box::into_pin(session.lock().await.try_stop()).await;
+        }
     }
 }
