498d9920d4e877112f328c554ab8b1720c68da15
MDario123 <manuel.dario.oliver@gmail.com>
2025-01-09 20:28:34 +0100

    add a package for refact cli

--- flake.nix
+++ flake.nix
@@ -26,6 +26,22 @@
           ];
         };
 
+      packages."x86_64-linux".refact = pkgs.python3Packages.buildPythonPackage {
+        pname = "refact";
+        version = "0.9.9";
+
+        src = ./python_binding_and_cmdline;
+
+        nativeBuildInputs = [
+          self.packages."x86_64-linux".refact-lsp
+        ];
+
+        postPatch = ''
+          mkdir -p ./refact/bin
+          cp ${self.packages."x86_64-linux".refact-lsp}/bin/refact-lsp ./refact/bin/refact-lsp
+        '';
+      };
+
       packages."x86_64-linux".default = self.packages."x86_64-linux".refact-lsp;
 
       devShells."x86_64-linux".default =
