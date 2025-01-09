0f7b5e2aa7e05c4782dad180d41a5c9f18b9d03e
MDario123 <manuel.dario.oliver@gmail.com>
2025-01-09 20:27:50 +0100

    make refact-lsp as a different package and then set it as the default

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
-      packages."x86_64-linux".default =
+      packages."x86_64-linux".refact-lsp =
         pkgs.rust.packages.stable.rustPlatform.buildRustPackage {
           name = "refact-lsp";
           src = ./.;
@@ -25,6 +25,8 @@
             openssl
           ];
         };
+
+      packages."x86_64-linux".default = self.packages."x86_64-linux".refact-lsp;
 
       devShells."x86_64-linux".default =
         (self.packages."x86_64-linux".default).overrideAttrs (self: super: {
