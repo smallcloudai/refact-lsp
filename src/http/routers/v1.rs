dc08e09d1a2633b90fd1ff9a852b1b35f41a13b9
JegernOUTT <sergey.vakhreev@gmail.com>
2025-01-14 14:49:25 +1030

    killing mem-list

--- src/http/routers/v1.rs
+++ src/http/routers/v1.rs
@@ -42,7 +42,7 @@ use crate::http::routers::v1::system_prompt::handle_v1_prepend_system_prompt_and
 #[cfg(feature="vecdb")]
 use crate::http::routers::v1::vecdb::{handle_v1_vecdb_search, handle_v1_vecdb_status};
 #[cfg(feature="vecdb")]
-use crate::http::routers::v1::handlers_memdb::{handle_mem_query, handle_mem_add, handle_mem_erase, handle_mem_update_used, handle_mem_block_until_vectorized, handle_mem_list};
+use crate::http::routers::v1::handlers_memdb::{handle_mem_query, handle_mem_add, handle_mem_erase, handle_mem_update_used, handle_mem_block_until_vectorized};
 use crate::http::routers::v1::v1_integrations::{handle_v1_integration_get, handle_v1_integration_icon, handle_v1_integration_save, handle_v1_integration_delete, handle_v1_integrations, handle_v1_integrations_filtered};
 use crate::agent_db::db_cthread::{handle_db_v1_cthread_update, handle_db_v1_cthreads_sub};
 use crate::agent_db::db_cmessage::{handle_db_v1_cmessages_update, handle_db_v1_cmessages_sub};
@@ -166,8 +166,7 @@ pub fn make_v1_router() -> Router {
         .route("/mem-erase", telemetry_post!(handle_mem_erase))
         .route("/mem-update-used", telemetry_post!(handle_mem_update_used))
         .route("/mem-block-until-vectorized", telemetry_get!(handle_mem_block_until_vectorized))
-        .route("/mem-list", telemetry_get!(handle_mem_list))
-        .route("/mem-sub", telemetry_get!(handle_mem_sub))
+        .route("/mem-sub", telemetry_post!(handle_mem_sub))
         ;
 
     builder.layer(CorsLayer::very_permissive())
