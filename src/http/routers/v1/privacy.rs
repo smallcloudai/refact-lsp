2c9dde232a47aae571431a6feb065e254698cb50
mitya <dimitry.ageev@gmail.com>
2025-01-16 11:45:35 +0100

    add dummy global flag on set and get handlers of privacy

--- src/http/routers/v1/privacy.rs
+++ src/http/routers/v1/privacy.rs
@@ -2,16 +2,30 @@ use std::sync::Arc;
 use axum::Extension;
 use axum::http::{Response, StatusCode};
 use hyper::Body;
+use serde::Deserialize;
 use tokio::sync::RwLock as ARwLock;
 use crate::custom_error::ScratchError;
 use crate::global_context::GlobalContext;
 use crate::privacy::{FilePrivacySettings, set_privacy_rules};
 
+#[derive(Deserialize)]
+struct PrivacyGet {
+    pub global: bool,
+}
+
 pub async fn handle_v1_privacy_get(
     Extension(gcx): Extension<Arc<ARwLock<GlobalContext>>>,
-    _: hyper::body::Bytes,
+    body_bytes: hyper::body::Bytes,
 ) -> axum::response::Result<Response<Body>, ScratchError> {
-    let privacy_rules = crate::privacy::load_privacy_rules_if_needed(gcx.clone()).await;
+    let get = serde_json::from_slice::<PrivacyGet>(&body_bytes)
+        .map_err(|e| ScratchError::new(StatusCode::UNPROCESSABLE_ENTITY, format!("JSON problem: {}", e)))?;
+    let privacy_rules;
+    if get.global {
+        privacy_rules = crate::privacy::load_privacy_rules_if_needed(gcx.clone()).await;
+    } else {
+        // TODO: local privacy.yaml
+        privacy_rules = Arc::new(FilePrivacySettings::default());
+    }
     let payload = serde_json::to_string_pretty(&privacy_rules).map_err(|e| {
         ScratchError::new(StatusCode::INTERNAL_SERVER_ERROR, format!("Failed to serialize payload: {}", e))
     })?;
@@ -22,13 +36,23 @@ pub async fn handle_v1_privacy_get(
         .unwrap())
 }
 
+#[derive(Deserialize)]
+struct PrivacySet {
+    pub privacy_rules: FilePrivacySettings,
+    pub global: bool,
+}
+
 pub async fn handle_v1_privacy_set(
     Extension(gcx): Extension<Arc<ARwLock<GlobalContext>>>,
     body_bytes: hyper::body::Bytes,
 ) -> axum::response::Result<Response<Body>, ScratchError> {
-    let post = serde_json::from_slice::<FilePrivacySettings>(&body_bytes)
+    let post = serde_json::from_slice::<PrivacySet>(&body_bytes)
         .map_err(|e| ScratchError::new(StatusCode::UNPROCESSABLE_ENTITY, format!("JSON problem: {}", e)))?;
-    set_privacy_rules(gcx.clone(), post).await;
+    if post.global {
+        set_privacy_rules(gcx.clone(), post.privacy_rules).await;
+    } else {
+        // TODO: local privacy.yaml
+    }
     Ok(Response::builder()
        .status(StatusCode::OK)
        .header("Content-Type", "application/json")
